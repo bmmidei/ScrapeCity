b083d38da182b4d4905483eccc7277c08c98ac51
pmt210 <brian.midei@gmail.com>
2019-01-13 22:58:32 -0500

    Added parsing for building ID

--- src/parse.py
+++ src/parse.py
@@ -1,16 +1,19 @@
 import re
 
-def parse_availTable(table):
+def parse_availTable(table, bldg_id):
     # Parse the available units on a given apartment buildings dedicated page
     apt_info = []
     apts = table.find_all('tr', {'class': 'rentalGridRow'})
     for apt in apts:
         info = {}
+        rentalkey = parse_rentalkey(apt)
         min_rent, max_rent = parse_rent(apt)
         beds = parse_beds(apt)
         baths = parse_baths(apt)
         min_area, max_area = parse_area(apt)
 
+        info['rentalkey'] = rentalkey
+        info['bldg_id'] = bldg_id
         info['min_rent'] = min_rent
         info['max_rent'] = max_rent
         info['beds'] = beds
@@ -22,24 +25,38 @@ def parse_availTable(table):
 
     return apt_info
 
+def parse_rentalkey(apt):
+    # Extract the unique rentalkey (ID)
+    return apt['data-rentalkey']
 
 def parse_rent(apt):
+    '''
+    Extract the rental rate - slightly complicated because some units are
+    listed with a high and low price
+    :param apt: BeautifulSoup object for the specific apartment
+    :return: min and max rent
+    '''
     try:
         rent = apt.find('td', {'class': 'rent'}).get_text()
         rent = re.sub('[$,]', '', rent)
+
+        # If rent is provided as a range
         if '-' in rent:
             min_rent, max_rent = rent.split('-')
             min_rent = int(min_rent)
             max_rent = int(max_rent)
+
+        # if rent is a single value, set min = max
         else:
             min_rent, max_rent = int(rent), int(rent)
     except:
+        # Some units have no rent listed
         return ('NA', 'NA')
 
     return (min_rent, max_rent)
 
-
 def parse_beds(apt):
+    # Extract the number of bedrooms
     try:
         beds = apt.find('td', {'class': 'beds'})
         beds = beds.find('span', {'class': 'shortText'}).get_text()
@@ -48,8 +65,8 @@ def parse_beds(apt):
 
     return beds.strip()
 
-
 def parse_baths(apt):
+    # Extract the number of bathrooms
     try:
         baths = apt.find('td', {'class': 'baths'})
         baths = baths.find('span', {'class': 'shortText'}).get_text()
@@ -58,8 +75,8 @@ def parse_baths(apt):
 
     return baths.strip()
 
-
 def parse_area(apt):
+    # Extract square footage
     try:
         area = apt.find('td', {'class': 'sqft'}).get_text()
         area = re.sub('[SqFt]', '', area)
@@ -68,12 +85,12 @@ def parse_area(apt):
             min_area = int(min_area)
             max_area = int(max_area)
         else:
-            min_area, max_area = int(min_area), int(max_area)
+            min_area, max_area = int(area), int(area)
     except:
         return ('NA', 'NA')
 
     return (min_area, max_area)
 
-
-def parse_bldgInfo():
-    pass
+def parse_bldgInfo(soup):
+    # TODO
+    return None
